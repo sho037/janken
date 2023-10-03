6972da38844572358e6a0642b0df3354301fac26
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-03 19:12:33 +0900

    じゃんけんができるように

--- src/main/java/oit/is/z2001/kaizi/janken/controller/Janken.java
+++ src/main/java/oit/is/z2001/kaizi/janken/controller/Janken.java
@@ -18,4 +18,46 @@ public class Janken {
     model.addAttribute("name", name);
     return "janken.html";
   }
+
+  @PostMapping("/janken/start")
+  public String jankenStart(@RequestParam String player_hand, ModelMap model) {
+    String[] hand_list = { "グー", "チョキ", "パー" };
+    String[] result_list = { "あいこ", "勝ち", "負け" };
+    // String cpu_hand = hand[(int) (Math.random() * 3)];
+    String cpu_hand = hand_list[0];
+
+    String result = result_list[0];
+
+    if (player_hand.equals("グー")) {
+      if (cpu_hand.equals("グー")) {
+        result = result_list[0];
+      } else if (cpu_hand.equals("チョキ")) {
+        result = result_list[1];
+      } else if (cpu_hand.equals("パー")) {
+        result = result_list[2];
+      }
+    } else if (player_hand.equals("チョキ")) {
+      if (cpu_hand.equals("グー")) {
+        result = result_list[2];
+      } else if (cpu_hand.equals("チョキ")) {
+        result = result_list[0];
+      } else if (cpu_hand.equals("パー")) {
+        result = result_list[1];
+      }
+    } else if (player_hand.equals("パー")) {
+      if (cpu_hand.equals("グー")) {
+        result = result_list[1];
+      } else if (cpu_hand.equals("チョキ")) {
+        result = result_list[2];
+      } else if (cpu_hand.equals("パー")) {
+        result = result_list[0];
+      }
+    }
+
+    model.addAttribute("player_hand", player_hand);
+    model.addAttribute("cpu_hand", cpu_hand);
+    model.addAttribute("result", result);
+
+    return "janken.html";
+  }
 }
