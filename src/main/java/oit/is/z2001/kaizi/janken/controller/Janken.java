1b7007cf6a744d2942a1fbae2631d6c07ce0fb72
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-03 18:52:30 +0900

    ユーザ名を入力し，janken.htmlを表示するように

---
+++ src/main/java/oit/is/z2001/kaizi/janken/controller/Janken.java
@@ -0,0 +1,21 @@
+package oit.is.z2001.kaizi.janken.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Janken {
+  @GetMapping("/janken")
+  public String janken() {
+    return "janken.html";
+  }
+
+  @PostMapping("/janken")
+  public String janken(@RequestParam String name, ModelMap model) {
+    model.addAttribute("name", name);
+    return "janken.html";
+  }
+}
