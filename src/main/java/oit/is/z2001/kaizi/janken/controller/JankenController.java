c4ee8cd55a67583135a4eccc98b1c53466f7bdbb
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-17 11:59:36 +0900

    ログインしているユーザーを表示できるように

--- src/main/java/oit/is/z2001/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2001/kaizi/janken/controller/JankenController.java
@@ -1,13 +1,21 @@
 package oit.is.z2001.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z2001.kaizi.janken.model.Entry;
+
 @Controller
 public class JankenController {
+  @Autowired
+  private Entry entry;
+
   @GetMapping("/janken")
   public String janken() {
     return "janken.html";
@@ -57,6 +65,15 @@ public class JankenController {
     model.addAttribute("player_hand", player_hand);
     model.addAttribute("cpu_hand", cpu_hand);
     model.addAttribute("result", result);
+
+    return "janken.html";
+  }
+
+  @GetMapping("/janken/entry")
+  public String entry(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.entry.addUser(loginUser);
+    model.addAttribute("entry", this.entry);
 
     return "janken.html";
   }
