a59fb4e833dd2b8f6538ff3df990d6861e5c82cf
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-17 11:26:55 +0900

    ファイル名変更

--- src/main/java/oit/is/z2001/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2001/kaizi/janken/controller/JankenController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Janken {
+public class JankenController {
   @GetMapping("/janken")
   public String janken() {
     return "janken.html";
