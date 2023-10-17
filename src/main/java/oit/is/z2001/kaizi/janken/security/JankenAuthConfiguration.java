4565481b2c24161dbad7e5e941d2912d6c055ba4
sho037 <103867061+sho037@users.noreply.github.com>
2023-10-17 11:24:29 +0900

    ユーザーの認証処理を作成

---
+++ src/main/java/oit/is/z2001/kaizi/janken/security/JankenAuthConfiguration.java
@@ -0,0 +1,63 @@
+package oit.is.z2001.kaizi.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class JankenAuthConfiguration {
+  /**
+   * 認証・認可に関する設定を行う
+   *
+   * @param http
+   * @throws Exception
+   */
+  @Bean
+  public SecurityFilterChain fileterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/"))
+        .authorizeHttpRequests(authz -> authz
+            // /janken/以下は認証済みであること
+            .requestMatchers("/janken/**").authenticated()
+            .requestMatchers("/**").permitAll()) // それ以外は全員アクセス可能
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")))
+        .headers(headers -> headers
+            .frameOptions(frameOptions -> frameOptions
+                .sameOrigin()));
+    return http.build();
+  }
+
+  /**
+   * 認証処理に関する設定（誰がどのようなロールでログインできるか）
+   */
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    // ユーザ名，パスワード，ロールを指定してbuildする
+    // このときパスワードはBCryptでハッシュ化されているため，{bcrypt}とつける
+    // ハッシュ化せずに平文でパスワードを指定する場合は{noop}をつける
+    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
+    // $ sshrun htpasswd -nbBC 10 user1 p@ss
+
+    UserDetails user1 = User.withUsername("user1")
+        .password("{bcrypt}$2y$10$7waDF9G/Nny4mSvc.lgCxeI.y.TswOZtW1qcWeCV8ehNieVow8s9S").roles("USER").build();
+    UserDetails user2 = User.withUsername("user2")
+        .password("{bcrypt}$2y$10$K/S5rZSCX6LMib606LR7J.Z7mjXHkiifA2DOtovm3KIEJkfDNzPJi").roles("USER").build();
+    UserDetails admin = User.withUsername("admin")
+        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("ADMIN").build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(user1, user2, admin);
+  }
+}
